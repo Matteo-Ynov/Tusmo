66d45af9524d5baa8c64cee8ad1eaeedee592179
AVELLA <arthur.vella@ynov.com>
2022-03-01 11:41:42 +0100

    add wordList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var lengthWord = 8;
+  var words = require("an-array-of-french-words").filter(
+    (d) => d.length === lengthWord
+  );
+
+  var choosenWord = words[Math.floor(Math.random() * words.length)];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{choosenWord}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
