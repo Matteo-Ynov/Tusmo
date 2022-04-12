ed2226fa006bc57c51d2a22b7bd932213bef1d0a
@matteo.christophe <matteo.christophe@opquast.com>
2022-04-12 10:32:48 +0200

    added end game screen

--- src/components/losePanel.jsx
+++ src/components/losePanel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { SutomContext } from "../states/sutomProvider";
 
 const LosePanel = () => {
   return (
--- src/components/winPanel.jsx
+++ src/components/winPanel.jsx
@@ -5,10 +5,13 @@ import { SutomContext } from "../states/sutomProvider";
 
 const WinPanel = () => {
   const [state, dispatch] = useContext(SutomContext);
-  console.log(state.hints);
+  createhintsarray(state.hints);
   return (
     <div className="panel">
       <h1>V I C T O R Y</h1>
+      <h1 className="tries">{
+        createhintsarray(state.hints).map(e => <p>{e}</p>)
+      }</h1>
       <Link
         class="btn btn-white btn-big"
         onClick={() => window.location.reload()}
@@ -19,5 +22,24 @@ const WinPanel = () => {
     </div>
   );
 };
+
+
+function createhintsarray(hints) {
+  let hintsarray = []
+  for (const hint of hints) {
+    let show = ""
+    for (const value of hint) {
+      if (value == "well-placed") {
+        show += "🟥"
+      } else if (value == "misplaced") {
+        show += "🟠"
+      } else {
+        show += "⚫"
+      }
+    }
+    hintsarray.push(show)
+  }
+  return hintsarray
+}
 
 export default WinPanel;
