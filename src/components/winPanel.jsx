d84ccf51f26c0f050aceefca5bc63f85c48aba4c
AVELLA <arthur.vella@ynov.com>
2022-04-12 09:50:54 +0200

    a

--- src/components/winPanel.jsx
+++ src/components/winPanel.jsx
@@ -1,12 +1,21 @@
 import React, { useContext } from "react";
 import { RESET } from "../states/wordsReducer";
 import { Link } from "react-router-dom";
+import { SutomContext } from "../states/sutomProvider";
 
-const WinPanel = ({ dispatch }) => {
+const WinPanel = () => {
+  const [state, dispatch] = useContext(SutomContext);
+  console.log(state.hints);
   return (
     <div className="panel">
       <h1>V I C T O R Y</h1>
-      <Link class="btn btn-white btn-big" onClick={() => window.location.reload()} to="/game">Play Again</Link>
+      <Link
+        class="btn btn-white btn-big"
+        onClick={() => window.location.reload()}
+        to="/game"
+      >
+        Play Again
+      </Link>
     </div>
   );
 };
