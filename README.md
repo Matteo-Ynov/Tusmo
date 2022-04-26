37ff403dce6a76b549e5c262465945fdef6c1e29
@matteo.christophe <matteo.christophe@opquast.com>
2022-04-26 10:15:38 +0200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-## Tusmo
-
-Jeu du motus en ligne proposant différents niveaux de difficultés, allant de la taille du mot au nombre d'indices.
-
-Les mots sont choisis à partir d'une liste prédéfini.
---
+++ precache-manifest.e6462d07c5d0948144fc83fc1d599842.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "16b0e950ae7bb3f5e00d",
+    "url": "/Tusmo/static/css/main.24eebf5f.chunk.css"
+  },
+  {
+    "revision": "16b0e950ae7bb3f5e00d",
+    "url": "/Tusmo/static/js/main.16b0e950.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/Tusmo/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "6d5d2417a0244067339a",
+    "url": "/Tusmo/static/js/2.6d5d2417.chunk.js"
+  },
+  {
+    "revision": "b5a866eb7131cefea26f",
+    "url": "/Tusmo/static/js/runtime~main.b5a866eb.js"
+  },
+  {
+    "revision": "d9a72a92ea1ab7b9db8f1bef967bbe38",
+    "url": "/Tusmo/static/media/correctWords.d9a72a92.txt"
+  },
+  {
+    "revision": "e12e32edddca498e93f284d07bca006b",
+    "url": "/Tusmo/static/media/commuter-sans-light.e12e32ed.otf"
+  },
+  {
+    "revision": "ece0d8f0192f933f7fbf32017b36af2c",
+    "url": "/Tusmo/index.html"
+  }
+];
